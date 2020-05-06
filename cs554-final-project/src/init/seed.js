637960c1de8381dd9e25b7872a8bd3dbb4446832
hughli <hughli@live.com>
2020-05-05 23:24:05 -0400

    add seed with data, create data/property

---
+++ cs554-final-project/src/init/seed.js
@@ -0,0 +1,136 @@
+const property = require("../data/handleProperty");
+const user = require("../data/user");
+
+
+//  ==================== User and Property data ====================
+
+user1 = {
+    username: "hill",
+    password:"mypass321",
+    photo:"../img/", //TODO
+    phone:"1029784458",
+    email:"hill@stevens.edu"
+}
+
+property1 = {
+    ownerid:null,
+    title:"1B1B house",
+    decsription:"If you're moving to Weehawken, NJ, this great property could be your new address. Located at 25 Hackensack Plank Rd in Weehawken, this property is perfectly situated for work or play. End your search and get moving. To learn more about this property, schedule a showing.",
+    address:{
+        street:"25 Hackensack Plank Rd",
+        city:"Weehawken",
+        state:"NJ",
+        postcode:"07086"
+    },
+    price:2600,
+    deposit:3600,
+    publishedDate:"2020-03-04",
+    bedroom:2,
+    baths:2,
+    area:1000
+}
+// ------------------------------------------------------------
+user2 = {
+    username: "zheng",
+    password:"test3321",
+    photo:"../img/", //TODO
+    phone:"1538649479",
+    email:"zheng@me.com"
+}
+
+property2 = {
+    ownerid:null,
+    title:"Little house",
+    decsription:"In the heart of Jersey City Greenville ! Beautiful 3 bedroom home for rent, renovated kitchens, baths and ceramic tiled floors!. Nice size bathrooms, high ceilings, all new appliances. 1.5 Month Security deposit. ALL UTILITIES ARE SEPARATE.",
+    address:{
+        street:"112 Bidwell Ave",
+        city:"Jersey City",
+        state:"NJ",
+        postcode:"07305"
+    },
+    price:1800,
+    deposit:3600,
+    publishedDate:"2020-03-06",
+    bedroom:2,
+    baths:2,
+    area:1200
+}
+
+// ------------------------------------------------------------
+user3 = {
+    username: "yong",
+    password:"testtest",
+    photo:"../img/", //TODO
+    phone:"8489798468",
+    email:"yongyongyong@qq.com"
+}
+
+property3 = {
+    ownerid:null,
+    title:"1708 West St",
+    decsription:"If you're moving to Union City, NJ, this great property could be your new rental. You'll enjoy this great location at 1708 West St in Union City and the attractions nearby. Stop your search and find your new home. Reach out to schedule a personal showing.",
+    address:{
+        street:"1708 West St",
+        city:"Union City",
+        state:"NJ",
+        postcode:"07087"
+    },
+    price:2100,
+    deposit:3600,
+    publishedDate:"2020-03-04",
+    bedroom:2,
+    baths:2,
+    area:800
+}
+// ------------------------------------------------------------
+user4 = {
+    username: "zhezhe",
+    password:"mypass323",
+    photo:"../img/", //TODO
+    phone:"6458859931",
+    email:"zhe@gmail.com"
+}
+
+property4 = {
+    ownerid:null,
+    title:"Little house",
+    decsription:"If you're moving to Weehawken, NJ, this great property could be your new address. Located at 25 Hackensack Plank Rd in Weehawken, this property is perfectly situated for work or play. End your search and get moving. To learn more about this property, schedule a showing.",
+    address:{
+        street:"168 Belmont Ave",
+        city:"Jersey City",
+        state:"NJ",
+        postcode:"07304"
+    },
+    price:2500,
+    deposit:3600,
+    publishedDate:"2020-01-04",
+    bedroom:2,
+    baths:2,
+    area:1000
+}
+// ------------------------------------------------------------
+user5 = {
+    username: "nealnealnealnealneal",
+    password:"iamaverylonglonglonglonglongpassword",
+    photo:"../img/", //TODO
+    phone:"1147895642",
+    email:"neal@live.com"
+}
+
+property5 = {
+    ownerid:null,
+    title:"Willow View",
+    decsription:"NO BROKER FEE!! An easy walk to vibrant Hoboken shopping and nightlife...Beautifully landscaped urban buildings...Friendly small-town living environment of Hoboken. Our property is conveniently located near bus and ferry to NYC, stores, bars and restaurants. Units include the following great amenities...heat, cooking gas and hot water included in rent. Please call or e-mail to schedule a viewing today!",
+    address:{
+        street:"1203 Willow Ave",
+        city:"Hoboken",
+        state:"NJ",
+        postcode:"07030"
+    },
+    price:2970,
+    deposit:4600,
+    publishedDate:"2019-06-29",
+    bedroom:3,
+    baths:1,
+    area:1402
+}
