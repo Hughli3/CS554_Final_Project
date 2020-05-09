191634d91932857c867f439c9065feebad9e82ae
hughli <hughli@live.com>
2020-05-09 01:29:40 -0400

    add react watch list with its component

--- cs554-final-project/client/src/componments/account/Watchlist.js
+++ cs554-final-project/client/src/componments/account/Watchlist.js
@@ -1,37 +1,54 @@
-import React, {Component} from 'react';
-import NoMatch from "../NoMatch"
+import React, { useEffect, useState } from "react"
+import WatchListProperty from "./WatchlistProperty"
 
-class AccountWatchlist extends Component{
-    constructor(props){
-        super(props)
-        this.state = {notFind:false, loading:true}
-    }
 
-    async getUser() {
-        try {
-            this.setState({loading:false})
-        } catch (e) {
-            this.setState({notFind:true})
+let myWatchList =  [
+        {
+            uid:123456,
+            title:"1125 Jefferson",
+            price:3600,
+            bedroom:3,
+            contact:"email@stevens.edu"
+        },
+        {
+            uid:654321,
+            title:"1125 Hason",
+            price:3600,
+            bedroom:3,
+            contact:"email@gmail.com"
+        },
+        {
+            uid:456123,
+            title:"3215 Hason",
+            price:2600,
+            bedroom:2,
+            contact:"email@gmail.com"
         }
-    }
+    ]
+export default function WatchList() {
+    const [currentUser, setCurrentUser] = useState(null);
+    const [pending, setPending] = useState(true);
+    const [property, setProperty] = useState(props.WatchList);
 
-    componentDidMount() {
-        this.getUser();
-    }
+    useEffect(() => {
+        console.log("WatchList has ben called")
+        app.auth().onAuthStateChanged((user) => {
+          setCurrentUser(user);
+          setPending(false);
+          setProperty({
+            uid:123456,
+            title:"1125 Jefferson",
+            price:3600,
+            bedroom:3,
+            contact:"email@stevens.edu"
+          });
+        });
+      }, []);
 
-    render() {
-        if(this.state.loading){
-            return <div><p>Loading...</p></div>;
-        } else if(this.state.notFind){
-            return <NoMatch />
-        } else {
-            return (
-                <div className='App-body'>
-                    <h1 className='cap-first-letter'> AccountWatchlist </h1>
-                </div>
-            );
-        }
-    }
+    const watchListComponents = watchList.map(listProperty =>  <WatchListProperty key={listProperty.uid} property = {listProperty} />)
+    return (
+        <div>
+            {watchListComponents}
+        </div>
+    )
 }
-
-export default AccountWatchlist;
