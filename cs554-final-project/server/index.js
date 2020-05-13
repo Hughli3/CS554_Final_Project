118f2bf42ae3542bdf8ddf32d7757aad6523ac76
queyong1996 <quey@sou.edu>
2020-05-12 21:32:55 -0500

    concurrently

--- cs554-final-project/server/index.js
+++ cs554-final-project/server/index.js
@@ -9,7 +9,7 @@ const configRoutes = require('./routes');
 app.use(express.json());
 configRoutes(app);
 
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log("We've now got a server!");
-  console.log('Your routes will be running on http://localhost:3000');
+  console.log('Your routes will be running on http://localhost:3001');
 });
