7376b454418f445b16c0e93d5b7046e891cac8d4
hughli <hughli@live.com>
2020-05-14 15:26:48 -0400

    change add property alert method

--- cs554-final-project/client/src/componments/account/AddProperty.js
+++ cs554-final-project/client/src/componments/account/AddProperty.js
@@ -2,9 +2,11 @@ import React, {Component, useContext, useState} from 'react';
 import { AuthContext } from "../auth/Auth";
 import serverController from '../../serverController';
 import { Redirect } from "react-router";
+import { useAlert } from 'react-alert'
 // import Select from 'react-select';
 
 const AddProperty = () => {
+    const alert = useAlert()
     const { currentUser } = useContext(AuthContext);
     const [isSuccess, setIsSuccess] = useState(false)
 
@@ -17,19 +19,26 @@ const AddProperty = () => {
       let time = new Date()
       data.date = time.toLocaleString("en", {hour12:false});
       console.log(data);
-      console.log(data.title)
       try {
         // TODO move these checker into function
-        if (data.description.value.length > 200) throw "description too long";
-        if (data.bedroom.value < 1 || data.bedroom.value > 10 ) throw "bedroom number invalid";
-        if (data.bath.value < 1|| data.bath.value > 10) throw "bath number invalid";
-        if (data.price.value < 0) throw "price invalid";
+        if (!data.title.value) throw "title not exist"
         if (data.title.value.length > 30) throw "title too long";
+        if(!data.description.value) throw "description not exist"
+        if (data.description.value.length > 200) throw "description too long";
+        if (!data.bedroom.value) throw "bedroom not exist"
+        if (data.bedroom.value < 1 || data.bedroom.value > 10 ) throw "bedroom number invalid";
+        if (!data.bath.value) throw "bath not exist"
+        if (data.bath.value < 1|| data.bath.value > 10) throw "bath number invalid";
+        if (!data.price.value) throw "price not exist"
+        if (data.price.value < 0) throw "price invalid";
+        if (!data.zipcode.value) throw "zipcode not exist"
         if (data.zipcode.value.length != 5) throw "zipcode invalid"
         await serverController.postProperty(currentUser, data)
         setIsSuccess(true)
+        alert.success('Signup sucessfully')
       } catch (error) {
-        alert(error);
+        // console.log(error);
+        alert.error(error)
       }
     };
 
@@ -45,7 +54,7 @@ const AddProperty = () => {
         <label htmlFor="title">Title</label>
         <input id="title" name="title" type="text" placeholder="title" />
         <label htmlFor="description">Description</label>
-        <input id="description" name="description" type="text" placeholder="description"  />
+        <textarea id="description" name="description" type="text" placeholder="description"  />
         <label htmlFor="type">Type</label>
         <select defaultValue="apartment" id="type" name="type">
           <option value="apartment">apartment</option>
