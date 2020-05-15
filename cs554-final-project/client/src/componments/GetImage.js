053e9a189550a4f7d8e46a8f729fe1b1cef1c381
queyong1996 <quey@sou.edu>
2020-05-15 15:15:23 -0500

    get Image test

--- cs554-final-project/client/src/componments/GetImage.js
+++ cs554-final-project/client/src/componments/GetImage.js
@@ -2,23 +2,17 @@ import React, {useState, useEffect} from 'react';
 import serverController from '../serverController';
 
 const GetImage = (props) => {
-    const [imageData, setImageData] = useState(undefined);
-    let sss = ""
-
+    const [imageData, setImageData] = useState({});
 
     useEffect(
 		() => {
 			async function fetchData() {
+                setImageData({});
                 const {data: resData} = await serverController.getImage(props.match.params.id);
-                console.log(resData.data)
-                sss = resData.data.toString()
-
-
-                console.log(sss);
+                console.log(resData)
                 
                 setImageData(resData);
                 console.log(imageData);
-                
 			}
 			fetchData();
 		},
