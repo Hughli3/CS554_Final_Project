e70e218413c467039c68475dad3fbce9e68102e8
nealxinyu <nealxinyu@gmail.com>
2020-05-15 00:51:29 -0400

    account page

--- cs554-final-project/client/src/componments/account/Account.js
+++ cs554-final-project/client/src/componments/account/Account.js
@@ -28,24 +28,41 @@ export default function Account(){
             <div class="container">
                 {/* <h1 className='mb-5'>Account</h1> */}
                 <div className="row">
-                    <div className="col-md-4 col-12">
-                        <h1>{currentUser.email}</h1>
-                        <p>Use Uid: {currentUser.uid}</p>
-               
+                    <div className="col-md-3 col-12">
+                        <div class="avatar-container">
+                            <img src="{{#if user.avatar}}{{user.avatar}}{{else}}/public/img/avatar/default-user.png{{/if}}" id="user-avatar" class="img-fluid avatar" alt="user avatar" width="300" />                
+                        </div>
+                        {currentUser.email ? (
+					        <div class="icon-group mt-4">
+                                <p>
+                                    <i class="fas fa-user"></i>{currentUser.email} 
+                                </p>
+                            </div>) : null}
+                        {currentUser.phone ? (
+                        <div class="icon-group my-3">
+                            <p>
+                                <i class="fas fa-phone"></i>{currentUser.phone}
+                            </p>
+                        </div>) : null}
                         <div>
                             <Link className='' to='/account/watchlist'>
-                                <button className="btn m-3 w-100 btn-secondary">my watchlist</button>
+                                <button className="btn my-3 w-100 btn-secondary">My watchlist</button>
                             </Link>
                         </div>
                         <div>
-                            <Link className='' to='/account/property'>
-                                <button className="btn m-3 w-100 btn-secondary">my property</button>
+                            <Link className='' to='/account'>
+                                <button className="btn my-3 w-100 btn-secondary">My property</button>
+                            </Link>
+                        </div>
+                        <div>
+                            <Link className='' to='/account/edit'>
+                                <button className="btn my-3 w-100 btn-secondary">Edit profile</button>
                             </Link>
                         </div>
                     </div>
-                    <div className="col-md-8 col-12">
+                    <div className="col-md-9 col-12">
+                        <PrivateRoute exact path='/account/' component={Property}/>
                         <PrivateRoute exact path='/account/property/add' component={AddProperty}/>
-                        <PrivateRoute exact path='/account/property' component={Property}/>
                         <PrivateRoute exact path='/account/watchlist' component={Watchlist}/>
                         <PrivateRoute exact path='/account/property/:id' component={EditProperty}/>
                     </div>
