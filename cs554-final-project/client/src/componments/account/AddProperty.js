1ae99ede3d5753247000ca692469221f6ada01cf
nealxinyu <nealxinyu@gmail.com>
2020-05-16 01:21:46 -0400

    image upload client

--- cs554-final-project/client/src/componments/account/AddProperty.js
+++ cs554-final-project/client/src/componments/account/AddProperty.js
@@ -1,14 +1,92 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext, useState, useCallback} from 'react';
 import { AuthContext } from "../auth/Auth";
 import serverController from '../../serverController';
 import { Redirect } from "react-router";
 import { useAlert } from 'react-alert'
 import ReactTooltip from "react-tooltip";
+import {useDropzone} from 'react-dropzone'
 
 const AddProperty = (props) => {
     const alert = useAlert()
     const { currentUser } = useContext(AuthContext);
-    // const [isSuccess, setIsSuccess] = useState(false)
+    const [imageData, setImageData] = useState([]);
+        
+    const getbase64 = async(file) => {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = (event) => resolve([file.name, "fieldName",event.target.result]);
+            reader.onerror = reject
+        })
+    }
+
+    const getData = async (Files) => {
+        return Promise.all(Files.map(file => getbase64(file)))
+    }
+
+    const onDrop = useCallback(async(acceptedFiles, rejectedFiles) => {
+        for (let file of rejectedFiles) {
+          for (let error of file.errors) {
+            console.log(file.file.name + " : " + error.message)
+          }
+        }
+
+        let files = await getData(acceptedFiles);
+        // set state: add to previous state
+        setImageData(prevState => {
+          let array = prevState.concat(files)
+          // remove duplicate
+          let set = new Set()
+          for (let i = array.length - 1; i >= 0 ; i--) {
+            if (set.has(array[i][2])){
+              array.splice(i, 1);
+            } else {
+              set.add(array[i][2])
+            }
+          }
+          return array
+        })
+    }, [])
+
+    const removeImage = (idx) => {
+      console.log(idx)
+      setImageData(prevState => {
+        let array = [...prevState]
+        array.splice(idx, 1);
+        return array;
+      })
+    }
+
+    let preview = imageData && imageData.length > 0 && imageData.map((key, idx) => {
+      return (
+        <div className="col-3">
+          <div className="img-preview-container avatar-container">
+            <img class="img-fluid img-preview" src={key[2]} alt={key[0]} />
+            <button type="button" onClick={() => removeImage(idx)} data-idx={idx} class="btn btn-danger btn-sm btn-round btn-shadow btn-delete-preview position-absolute">delete</button>
+          </div>
+        </div>
+      ) || null;
+    });
+
+    const {getRootProps, getInputProps, isDragActive} = useDropzone({
+        onDrop,
+        accept: 'image/jpeg, image/png',
+        minSize: 0,
+        maxSize: 5242880,
+    })
+
+    const uploadImage = (
+      <>
+        <label htmlFor="album">Album</label>
+        <div {...getRootProps()} class="image-upload property-card property-add align-self-center d-flex align-items-center justify-content-center mb-3">
+          <input id="album" {...getInputProps()} />
+          <div>
+            <p><i class="fas fa-file-image"></i></p>
+            <p>Click here or drop to upload photos!</p>
+          </div>
+        </div>
+      </>
+    );
 
     const addProperty = async (event) => {
       event.preventDefault();
@@ -32,11 +110,10 @@ const AddProperty = (props) => {
         if (!data.zipcode.value) throw "zipcode not exist";
         if (data.zipcode.value.length != 5) throw "zipcode invalid";
         await serverController.postProperty(currentUser, data);
-        // setIsSuccess(true)
+
         props.history.push("/account")
         alert.success('post sucessfully')
       } catch (error) {
-        // console.log(error);
         alert.error(error)
       }
     };
@@ -50,19 +127,25 @@ const AddProperty = (props) => {
         <h1>Post Property</h1>
         <form onSubmit={addProperty}>
           <div className="row">
+
             <div class="col-md-12">
               <div class="form-group">
                 <label htmlFor="title">Title</label>
                 <input class="form-control" id="title" name="title" placeholder="title" data-tip="title length must less than 70"/>
               </div>
             </div>
+
             <div class="col-md-12">
               <div class="form-group">
                 <label htmlFor="description">Description</label>
                 <textarea id="description" rows="10" class="form-control" name="description" type="text" placeholder="description" data-tip="description length need to less than 200" />
               </div>
             </div>
-            
+
+            <div class="col-md-12">
+              {uploadImage}
+              {preview ?  <div className="row mb-2">{preview}</div> : null}
+            </div>
 
             <div class="col-md-12">
                 <label>Type</label>
