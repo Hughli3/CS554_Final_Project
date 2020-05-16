1c4fd0f83252c17f3ace09fc9d462779550c2534
nealxinyu <nealxinyu@gmail.com>
2020-05-15 22:41:07 -0400

    preview iamges

--- cs554-final-project/client/src/componments/Image.js
+++ cs554-final-project/client/src/componments/Image.js
@@ -1,14 +1,29 @@
-import React, { useCallback } from 'react';
+import React, {useState, useCallback, useEffect } from 'react';
 import serverController from '../serverController';
 import {useDropzone} from 'react-dropzone'
 
 const Image = (props) => {
+    const [imageData, setImageData] = useState([]);
+
+    let preview = imageData && imageData.map(key => {
+      console.log("trya")
+      // console.log(imageData[key])
+      return (<img src={key[2]} alt="test" />);
+    });
+
+    // useEffect(
+    //   () => {
+    //     console.log("a")
+          
+    //   },
+    //   [imageData]
+    // );
 
     const getbase64 = async(file) => {
         return new Promise((resolve, reject) => {
             const reader = new FileReader();
             reader.readAsDataURL(file);
-            reader.onload = (event) => resolve([file.name, event.target.result]);
+            reader.onload = (event) => resolve([file.name, "fieldName",event.target.result]);
             reader.onerror = reject
         })
     }
@@ -18,20 +33,36 @@ const Image = (props) => {
     }
 
     const onDrop = useCallback(async(acceptedFiles) => {
-        // console.log(acceptedFiles);
-
         let files = await getData(acceptedFiles);
-        const {data}  = await serverController.addImage(files)
-        console.log(data);
+        // set state: add to previous state
+        setImageData(prevState => {
+          let array = prevState.concat(files)
+          // remove duplicate
+          let set = new Set()
+          for (let i = array.length - 1; i >= 0 ; i--) {
+            if (set.has(array[i][2])){
+              array.splice(i, 1);
+            } else {
+              set.add(array[i][2])
+            }
+          }
+          return array
+        })
+
+        function removeDuplicates(array) {
+          return 
+        };
+        console.log(imageData)
         
+
+
+        // const {data} = await serverController.addImage(files)
     }, [])
 
     const {getRootProps, getInputProps, isDragActive} = useDropzone({
-                            onDrop,
-                            accept: 'image/jpeg, image/png'
-                        })
-
-
+        onDrop,
+        accept: 'image/jpeg, image/png'
+    })
 
     return (
         <div className="text-center mt-5">
@@ -43,6 +74,7 @@ const Image = (props) => {
                 <p>Click here or drop files to upload!</p>
             }
             </div>
+            {preview}
         </div>
     );
 };
